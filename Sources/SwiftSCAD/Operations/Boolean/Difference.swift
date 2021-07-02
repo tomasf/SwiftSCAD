10f55c1598224dfe1774f475217abbe47b848766
Tomas Franzén <tomas.franzen@bontouch.com>
2021-07-02 12:26:00 +0200

    Implement Difference for 2D

--- Sources/SwiftSCAD/Operations/Boolean/Difference.swift
+++ Sources/SwiftSCAD/Operations/Boolean/Difference.swift
@@ -7,22 +7,43 @@
 
 import Foundation
 
-public struct Difference3D: Geometry3D {
-	public let positive: Geometry3D
-	public let negative: Geometry3D
+struct Difference3D: Geometry3D {
+	let positive: Geometry3D
+	let negative: Geometry3D
 
-	public init(positive: Geometry3D, negative: Geometry3D) {
+	init(positive: Geometry3D, negative: Geometry3D) {
 		self.positive = positive
 		self.negative = negative
 	}
 
-	public func generateOutput(environment: Environment) -> String {
+	func generateOutput(environment: Environment) -> String {
 		let posCode = positive.generateOutput(environment: environment)
 		let negCode = negative.generateOutput(environment: environment)
 		return "difference() {\n\(posCode)\n\(negCode)\n}"
 	}
 }
 
-public func Difference(@UnionBuilder _ positive: () -> Geometry3D, @UnionBuilder subtracting negative: () -> Geometry3D) -> Difference3D {
+public func Difference(@UnionBuilder _ positive: () -> Geometry3D, @UnionBuilder subtracting negative: () -> Geometry3D) -> Geometry3D {
 	Difference3D(positive: positive(), negative: negative())
+}
+
+
+struct Difference2D: Geometry2D {
+	let positive: Geometry2D
+	let negative: Geometry2D
+
+	init(positive: Geometry2D, negative: Geometry2D) {
+		self.positive = positive
+		self.negative = negative
+	}
+
+	func generateOutput(environment: Environment) -> String {
+		let posCode = positive.generateOutput(environment: environment)
+		let negCode = negative.generateOutput(environment: environment)
+		return "difference() {\n\(posCode)\n\(negCode)\n}"
+	}
+}
+
+public func Difference(@UnionBuilder _ positive: () -> Geometry2D, @UnionBuilder subtracting negative: () -> Geometry2D) -> Geometry2D {
+	Difference2D(positive: positive(), negative: negative())
 }
