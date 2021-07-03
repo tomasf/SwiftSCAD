63b0cb8329693b89d1c65cff9653eb1bce585de6
Tomas Franzén <tomas.franzen@bontouch.com>
2021-07-04 00:17:13 +0200

    Generalize debug modifiers and add disabled

--- Sources/SwiftSCAD/DebugModifiers.swift
+++ Sources/SwiftSCAD/DebugModifiers.swift
@@ -7,88 +7,56 @@
 
 import Foundation
 
-struct Highlight3D: Geometry3D {
+struct Prefix3D: Geometry3D {
+	let prefix: String
 	let body: Geometry3D
 
 	func generateOutput(environment: Environment) -> String {
-		return "#" + body.generateOutput(environment: environment)
+		return prefix + body.generateOutput(environment: environment)
 	}
 }
 
-struct Highlight2D: Geometry2D {
+struct Prefix2D: Geometry2D {
+	let prefix: String
 	let body: Geometry2D
 
 	func generateOutput(environment: Environment) -> String {
-		return "#" + body.generateOutput(environment: environment)
+		return prefix + body.generateOutput(environment: environment)
 	}
 }
 
 public extension Geometry2D {
 	func highlighted() -> Geometry2D {
-		Highlight2D(body: self)
+		Prefix2D(prefix: "#", body: self)
+	}
+
+	func only() -> Geometry2D {
+		Prefix2D(prefix: "!", body: self)
+	}
+
+	func background() -> Geometry2D {
+		Prefix2D(prefix: "%", body: self)
+	}
+
+	func disabled() -> Geometry2D {
+		Prefix2D(prefix: "*", body: self)
 	}
 }
 
 public extension Geometry3D {
 	func highlighted() -> Geometry3D {
-		Highlight3D(body: self)
+		Prefix3D(prefix: "#", body: self)
 	}
-}
 
-
-struct Only3D: Geometry3D {
-	let body: Geometry3D
-
-	func generateOutput(environment: Environment) -> String {
-		return "!" + body.generateOutput(environment: environment)
-	}
-}
-
-struct Only2D: Geometry2D {
-	let body: Geometry2D
-
-	func generateOutput(environment: Environment) -> String {
-		return "!" + body.generateOutput(environment: environment)
-	}
-}
-
-public extension Geometry2D {
-	func only() -> Geometry2D {
-		Only2D(body: self)
-	}
-}
-
-public extension Geometry3D {
 	func only() -> Geometry3D {
-		Only3D(body: self)
+		Prefix3D(prefix: "!", body: self)
 	}
-}
 
-
-struct Background3D: Geometry3D {
-	let body: Geometry3D
-
-	func generateOutput(environment: Environment) -> String {
-		return "%" + body.generateOutput(environment: environment)
-	}
-}
-
-struct Background2D: Geometry2D {
-	let body: Geometry2D
-
-	func generateOutput(environment: Environment) -> String {
-		return "%" + body.generateOutput(environment: environment)
-	}
-}
-
-public extension Geometry2D {
-	func background() -> Geometry2D {
-		Background2D(body: self)
-	}
-}
-
-public extension Geometry3D {
 	func background() -> Geometry3D {
-		Background3D(body: self)
+		Prefix3D(prefix: "%", body: self)
+	}
+
+	func disabled() -> Geometry3D {
+		Prefix3D(prefix: "*", body: self)
 	}
 }
