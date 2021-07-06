8ba525a699a0b9d0f3007f1310a5af7d3a93c3c3
Tomas Franzén <tomas.franzen@bontouch.com>
2021-07-06 12:35:20 +0200

    Add facet modification for 2D

--- Sources/SwiftSCAD/Environment/Facets.swift
+++ Sources/SwiftSCAD/Environment/Facets.swift
@@ -7,35 +7,60 @@
 
 import Foundation
 
-public struct SetFacets: Geometry3D {
-	public let facets: Environment.Facets
+func facetModification(_ facets: Environment.Facets, body: Geometry, environment: Environment) -> String {
+	let variables: [String: String]
+
+	switch facets {
+	case .fixed (let count):
+		variables = ["fn": String(count)]
+	case .dynamic (let minAngle, let minSize):
+		variables = ["fa": minAngle.scadString, "fs": String(minSize), "fn": "0"]
+	}
+
+	let varString = variables.map { key, value in
+		"$\(key) = \(value); "
+	}.joined()
+
+	let newEnvironment = environment.withFacets(facets)
+	return "union() { \(varString) \(body.scadString(environment: newEnvironment)) }"
+}
+
+
+struct SetFacets3D: Geometry3D {
+	let facets: Environment.Facets
 	let body: Geometry3D
 
-	public func scadString(environment: Environment) -> String {
-		let variables: [String: String]
-
-		switch facets {
-		case .fixed (let count):
-			variables = ["fn": String(count)]
-		case .dynamic (let minAngle, let minSize):
-			variables = ["fa": minAngle.scadString, "fs": String(minSize), "fn": "0"]
-		}
-
-		let varString = variables.map { key, value in
-			"$\(key) = \(value); "
-		}.joined()
-
-		let newEnvironment = environment.withFacets(facets)
-		return "union() { \(varString) \(body.scadString(environment: newEnvironment)) }"
+	func scadString(environment: Environment) -> String {
+		facetModification(facets, body: body, environment: environment)
 	}
 }
 
 public extension Geometry3D {
 	func withFacets(minAngle: Angle, minSize: Double) -> Geometry3D {
-		SetFacets(facets: .dynamic(minAngle: minAngle, minSize: minSize), body: self)
+		SetFacets3D(facets: .dynamic(minAngle: minAngle, minSize: minSize), body: self)
 	}
 
 	func withFacets(count: Int) -> Geometry3D {
-		SetFacets(facets: .fixed(count), body: self)
+		SetFacets3D(facets: .fixed(count), body: self)
+	}
+}
+
+
+struct SetFacets2D: Geometry2D {
+	let facets: Environment.Facets
+	let body: Geometry2D
+
+	func scadString(environment: Environment) -> String {
+		facetModification(facets, body: body, environment: environment)
+	}
+}
+
+public extension Geometry2D {
+	func withFacets(minAngle: Angle, minSize: Double) -> Geometry2D {
+		SetFacets2D(facets: .dynamic(minAngle: minAngle, minSize: minSize), body: self)
+	}
+
+	func withFacets(count: Int) -> Geometry2D {
+		SetFacets2D(facets: .fixed(count), body: self)
 	}
 }
