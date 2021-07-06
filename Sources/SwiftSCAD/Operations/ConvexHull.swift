b48584aa64e595695df8d99dc6cba22fd9fa7154
Tomas Franzén <tomas.franzen@bontouch.com>
2021-07-06 10:19:50 +0200

    Rename hull() to convexHull()

---
+++ Sources/SwiftSCAD/Operations/ConvexHull.swift
@@ -0,0 +1,37 @@
+//
+//  ConvexHull.swift
+//  GeometryGenerator
+//
+//  Created by Tomas Franzén on 2021-06-29.
+//
+
+import Foundation
+
+struct ConvexHull3D: Geometry3D {
+	let body: Geometry3D
+
+	func scadString(environment: Environment) -> String {
+		"hull() { \(body.scadString(environment: environment)) }"
+	}
+}
+
+public extension Geometry3D {
+	func convexHull() -> Geometry3D {
+		ConvexHull3D(body: self)
+	}
+}
+
+
+struct ConvexHull2D: Geometry2D {
+	let body: Geometry2D
+
+	func scadString(environment: Environment) -> String {
+		"hull() { \(body.scadString(environment: environment)) }"
+	}
+}
+
+public extension Geometry2D {
+	func convexHull() -> Geometry2D {
+		ConvexHull2D(body: self)
+	}
+}
