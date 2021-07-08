8bcbb7329976348a65f7eb276384659c9f3b284e
Tomas Franzén <tomas.franzen@bontouch.com>
2021-07-08 19:28:28 +0200

    Fix affine transformation concatenation

--- Sources/SwiftSCAD/Operations/Transform.swift
+++ Sources/SwiftSCAD/Operations/Transform.swift
@@ -51,20 +51,38 @@ public struct AffineTransform {
 		scale(x: v.x, y: v.y, z: v.z)
 	}
 
-	public static func rotate(x: Angle = 0, y: Angle = 0, z: Angle = 0) -> AffineTransform {
+	private static func rotate(x: Angle) -> AffineTransform {
 		var transform = identity
-		transform[0, 0] = y.cos * z.cos
-		transform[0, 1] = -z.sin
-		transform[0, 2] = y.sin
-
-		transform[1, 0] = z.sin
-		transform[1, 1] = x.cos * z.cos
+		transform[1, 1] = x.cos
 		transform[1, 2] = -x.sin
-
-		transform[2, 0] = -y.sin
 		transform[2, 1] = x.sin
-		transform[2, 2] = x.cos * y.cos
+		transform[2, 2] = x.cos
 		return transform
+	}
+
+	private static func rotate(y: Angle) -> AffineTransform {
+		var transform = identity
+		transform[0, 0] = y.cos
+		transform[0, 2] = y.sin
+		transform[2, 0] = -y.sin
+		transform[2, 2] = y.cos
+		return transform
+	}
+
+	private static func rotate(z: Angle) -> AffineTransform {
+		var transform = identity
+		transform[0, 0] = z.cos
+		transform[0, 1] = -z.sin
+		transform[1, 0] = z.sin
+		transform[1, 1] = z.cos
+		return transform
+	}
+
+	public static func rotate(x: Angle = 0, y: Angle = 0, z: Angle = 0) -> AffineTransform {
+		return identity
+			.concatenated(with: rotate(x: x))
+			.concatenated(with: rotate(y: y))
+			.concatenated(with: rotate(z: z))
 	}
 
 	public static func rotate(_ a: [Angle]) -> AffineTransform {
@@ -72,12 +90,28 @@ public struct AffineTransform {
 		return rotate(x: a[0], y: a[1], z: a[2])
 	}
 
+	public static func shear(_ axis: Axis3D, along otherAxis: Axis3D, factor: Double) -> AffineTransform {
+		precondition(axis != otherAxis, "Shearing requires two distinct axes")
+
+		let order: [Axis3D] = [.x, .y, .z]
+		let row = order.firstIndex(of: axis)!
+		let column = order.firstIndex(of: otherAxis)!
+
+		var t = AffineTransform.identity
+		t[row, column] = factor
+		return t
+	}
+
 	public func concatenated(with other: AffineTransform) -> AffineTransform {
-		AffineTransform(values.enumerated().map { rowIndex, row in
-			row.enumerated().map { columnIndex, value in
-				value * other[rowIndex, columnIndex]
+		AffineTransform(
+			(0..<4).map { row in
+				(0..<4).map { column in
+					(0..<4).map { i in
+						other[row, i] * self[i, column]
+					}.reduce(0, +)
+				}
 			}
-		})
+		)
 	}
 
 	public func translate(x: Double = 0, y: Double = 0, z: Double = 0) -> AffineTransform {
@@ -102,6 +136,10 @@ public struct AffineTransform {
 
 	public func rotate(x: Angle = 0, y: Angle = 0, z: Angle = 0) -> AffineTransform {
 		concatenated(with: .rotate(x: x, y: y, z: z))
+	}
+
+	func shear(_ axis: Axis3D, along otherAxis: Axis3D, factor: Double) -> AffineTransform {
+		concatenated(with: .shear(axis, along: otherAxis, factor: factor))
 	}
 
 	public subscript(_ row: Int, _ column: Int) -> Double {
@@ -146,14 +184,6 @@ public extension Geometry3D {
 	}
 
 	func shear(_ axis: Axis3D, along otherAxis: Axis3D, factor: Double) -> Geometry3D {
-		precondition(axis != otherAxis, "Shearing requires two distinct axes")
-
-		let order: [Axis3D] = [.x, .y, .z]
-		let row = order.firstIndex(of: axis)!
-		let column = order.firstIndex(of: otherAxis)!
-
-		var t = AffineTransform.identity
-		t[row, column] = factor
-		return transform(t)
+		transform(.shear(axis, along: otherAxis, factor: factor))
 	}
 }
