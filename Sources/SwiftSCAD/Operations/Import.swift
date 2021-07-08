118c518745388fd41e1812168157c407690f2171
Tomas Franzén <tomas.franzen@bontouch.com>
2021-07-08 18:20:53 +0200

    Add convexity and layer to Import

--- Sources/SwiftSCAD/Operations/Import.swift
+++ Sources/SwiftSCAD/Operations/Import.swift
@@ -9,15 +9,20 @@ import Foundation
 
 public struct Import3D: Geometry3D {
 	let path: String
+	let convexity: Int
 
-	public init(path: String) {
+	public init(path: String, convexity: Int = 2) {
 		self.path = path
+		self.convexity = convexity
 	}
 
 	public func scadString(environment: Environment) -> String {
 		SCADCall(
 			name: "import",
-			params: ["file": path]
+			params: [
+				"file": path,
+				"convexity": convexity
+			]
 		)
 		.scadString(environment: environment)
 	}
@@ -25,15 +30,24 @@ public struct Import3D: Geometry3D {
 
 public struct Import2D: Geometry2D {
 	let path: String
+	let layer: String?
+	let convexity: Int
 
-	public init(path: String) {
+	public init(path: String, dxfLayer: String? = nil, convexity: Int = 2) {
 		self.path = path
+		self.layer = dxfLayer
+		self.convexity = convexity
 	}
 
 	public func scadString(environment: Environment) -> String {
-		SCADCall(
+		let params: [String: SCADValue?] = [
+			"file": path,
+			"layer": layer,
+			"convexity": convexity
+		]
+		return SCADCall(
 			name: "import",
-			params: ["file": path]
+			params: params.compactMapValues { $0 }
 		)
 		.scadString(environment: environment)
 	}
