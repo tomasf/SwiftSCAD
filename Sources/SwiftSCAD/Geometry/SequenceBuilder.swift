da6721b15bf9d4f57f385ae3b2430c5b7dc1a25c
Tomas Franzén <tomas@tomasf.se>
2021-07-11 23:22:11 +0200

    Make SequenceBuilder internal

--- Sources/SwiftSCAD/Geometry/SequenceBuilder.swift
+++ Sources/SwiftSCAD/Geometry/SequenceBuilder.swift
@@ -1,20 +1,20 @@
 import Foundation
 
-@resultBuilder public struct SequenceBuilder {
-	public static func buildBlock(_ children: Geometry3D...) -> [Geometry3D] {
+@resultBuilder struct SequenceBuilder {
+	static func buildBlock(_ children: Geometry3D...) -> [Geometry3D] {
 		return children
 	}
 
-	public static func buildIf(_ children: Geometry3D?...) -> [Geometry3D] {
+	static func buildIf(_ children: Geometry3D?...) -> [Geometry3D] {
 		return children.compactMap { $0 }
 	}
 
 
-	public static func buildBlock(_ children: Geometry2D...) -> [Geometry2D] {
+	static func buildBlock(_ children: Geometry2D...) -> [Geometry2D] {
 		return children
 	}
 
-	public static func buildIf(_ children: Geometry2D?...) -> [Geometry2D] {
+	static func buildIf(_ children: Geometry2D?...) -> [Geometry2D] {
 		return children.compactMap { $0 }
 	}
 }
