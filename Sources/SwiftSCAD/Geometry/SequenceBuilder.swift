e5ee72f86361821009e868c6c21405320da746dc
Tomas Franzén <tomas@tomasf.se>
2021-07-11 23:25:21 +0200

    Revert "Make SequenceBuilder internal"
    This reverts commit da6721b15bf9d4f57f385ae3b2430c5b7dc1a25c.

--- Sources/SwiftSCAD/Geometry/SequenceBuilder.swift
+++ Sources/SwiftSCAD/Geometry/SequenceBuilder.swift
@@ -1,20 +1,20 @@
 import Foundation
 
-@resultBuilder struct SequenceBuilder {
-	static func buildBlock(_ children: Geometry3D...) -> [Geometry3D] {
+@resultBuilder public struct SequenceBuilder {
+	public static func buildBlock(_ children: Geometry3D...) -> [Geometry3D] {
 		return children
 	}
 
-	static func buildIf(_ children: Geometry3D?...) -> [Geometry3D] {
+	public static func buildIf(_ children: Geometry3D?...) -> [Geometry3D] {
 		return children.compactMap { $0 }
 	}
 
 
-	static func buildBlock(_ children: Geometry2D...) -> [Geometry2D] {
+	public static func buildBlock(_ children: Geometry2D...) -> [Geometry2D] {
 		return children
 	}
 
-	static func buildIf(_ children: Geometry2D?...) -> [Geometry2D] {
+	public static func buildIf(_ children: Geometry2D?...) -> [Geometry2D] {
 		return children.compactMap { $0 }
 	}
 }
