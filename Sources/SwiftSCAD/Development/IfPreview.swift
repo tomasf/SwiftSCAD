93a731f45b4095f2bac27c4e1f4cc3d8ffcc3b8c
Tomas Franzén <tomas@tomasf.se>
2021-12-04 22:12:37 +0100

    Make Empty internal

--- Sources/SwiftSCAD/Development/IfPreview.swift
+++ Sources/SwiftSCAD/Development/IfPreview.swift
@@ -1,50 +1,84 @@
 import Foundation
 
 struct IfPreview3D: Geometry3D {
-	let previewBody: Geometry3D
-	let renderBody: Geometry3D
+	let previewBody: Geometry3D?
+	let renderBody: Geometry3D?
 
-	init(ifPreview previewBody: Geometry3D, ifRender renderBody: Geometry3D) {
+	init(ifPreview previewBody: Geometry3D? = nil, ifRender renderBody: Geometry3D? = nil) {
 		self.previewBody = previewBody
 		self.renderBody = renderBody
 	}
 
 	func scadString(in environment: Environment) -> String {
-		let previewCode = previewBody.scadString(in: environment)
-		let renderCode = renderBody.scadString(in: environment)
-		return "if ($preview) { \(previewCode) } else { \(renderCode) }"
+		let previewCode = previewBody?.scadString(in: environment)
+		let renderCode = renderBody?.scadString(in: environment)
+
+        if let preview = previewCode, let render = renderCode {
+            return "if ($preview) { \(preview) } else { \(render) }"
+        } else if let preview = previewCode {
+            return "if ($preview) { \(preview) }"
+        } else if let render = renderCode {
+            return "if (!$preview) { \(render) }"
+        } else {
+            preconditionFailure("previewBody and renderBody can't both be nil.")
+        }
 	}
 }
 
-public func IfPreview(@UnionBuilder _ preview: () -> Geometry3D, @UnionBuilder ifRender render: () -> Geometry3D = { Empty() }) -> Geometry3D {
-	IfPreview3D(ifPreview: preview(), ifRender: render())
+public func IfPreview(@UnionBuilder _ preview: () -> Geometry3D, @UnionBuilder ifRender render: () -> Geometry3D) -> Geometry3D {
+    IfPreview3D(ifPreview: preview(), ifRender: render())
 }
 
-public func IfRender(@UnionBuilder _ render: () -> Geometry3D, @UnionBuilder ifPreview preview: () -> Geometry3D = { Empty() }) -> Geometry3D {
-	IfPreview3D(ifPreview: preview(), ifRender: render())
+public func IfPreview(@UnionBuilder _ preview: () -> Geometry3D) -> Geometry3D {
+    IfPreview3D(ifPreview: preview())
+}
+
+public func IfRender(@UnionBuilder _ render: () -> Geometry3D, @UnionBuilder ifPreview preview: () -> Geometry3D) -> Geometry3D {
+    IfPreview3D(ifPreview: preview(), ifRender: render())
+}
+
+public func IfRender(@UnionBuilder _ render: () -> Geometry3D) -> Geometry3D {
+    IfPreview3D(ifRender: render())
 }
 
 
 struct IfPreview2D: Geometry2D {
-	let previewBody: Geometry2D
-	let renderBody: Geometry2D
+	let previewBody: Geometry2D?
+	let renderBody: Geometry2D?
 
-	init(ifPreview previewBody: Geometry2D, ifRender renderBody: Geometry2D) {
+	init(ifPreview previewBody: Geometry2D? = nil, ifRender renderBody: Geometry2D? = nil) {
 		self.previewBody = previewBody
 		self.renderBody = renderBody
 	}
 
-	func scadString(in environment: Environment) -> String {
-		let previewCode = previewBody.scadString(in: environment)
-		let renderCode = renderBody.scadString(in: environment)
-		return "if ($preview) { \(previewCode) } else { \(renderCode) }"
-	}
+    func scadString(in environment: Environment) -> String {
+        let previewCode = previewBody?.scadString(in: environment)
+        let renderCode = renderBody?.scadString(in: environment)
+
+        if let preview = previewCode, let render = renderCode {
+            return "if ($preview) { \(preview) } else { \(render) }"
+        } else if let preview = previewCode {
+            return "if ($preview) { \(preview) }"
+        } else if let render = renderCode {
+            return "if (!$preview) { \(render) }"
+        } else {
+            preconditionFailure("previewBody and renderBody can't both be nil.")
+        }
+    }
 }
 
-func IfPreview(@UnionBuilder _ preview: () -> Geometry2D, @UnionBuilder ifRender render: () -> Geometry2D = { Empty() }) -> Geometry2D {
-	IfPreview2D(ifPreview: preview(), ifRender: render())
+func IfPreview(@UnionBuilder _ preview: () -> Geometry2D, @UnionBuilder ifRender render: () -> Geometry2D) -> Geometry2D {
+    IfPreview2D(ifPreview: preview(), ifRender: render())
 }
 
-func IfRender(@UnionBuilder _ render: () -> Geometry2D, @UnionBuilder ifPreview preview: () -> Geometry2D = { Empty() }) -> Geometry2D {
-	IfPreview2D(ifPreview: preview(), ifRender: render())
+func IfPreview(@UnionBuilder _ preview: () -> Geometry2D) -> Geometry2D {
+    IfPreview2D(ifPreview: preview())
+}
+
+func IfRender(@UnionBuilder _ render: () -> Geometry2D, @UnionBuilder ifPreview preview: () -> Geometry2D) -> Geometry2D {
+    IfPreview2D(ifPreview: preview(), ifRender: render())
+}
+
+func IfRender(@UnionBuilder _ render: () -> Geometry2D) -> Geometry2D {
+    IfPreview2D(ifRender: render())
 }
--- Sources/SwiftSCAD/Geometry/Geometry.swift
+++ Sources/SwiftSCAD/Geometry/Geometry.swift
@@ -15,9 +15,8 @@ public protocol Geometry3D: Geometry {}
 public protocol Geometry2D: Geometry {}
 
 
-public struct Empty: Geometry3D, Geometry2D {
-	public init() {}
-	public func scadString(in environment: Environment) -> String {
+internal struct Empty: Geometry3D, Geometry2D {
+	func scadString(in environment: Environment) -> String {
 		""
 	}
 }
