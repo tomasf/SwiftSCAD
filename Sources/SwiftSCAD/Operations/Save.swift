6ff321aa1cfeee687c87f20f6f2025026384bae2
Tomas Franzén <tomas@tomasf.se>
2022-03-22 11:32:51 +0100

    Add save(to:) methods that accept a URL

--- Sources/SwiftSCAD/Operations/Save.swift
+++ Sources/SwiftSCAD/Operations/Save.swift
@@ -1,30 +1,41 @@
 import Foundation
 
-fileprivate func saveToFile(string: String, to path: String) {
-	let fullPath = (path as NSString).expandingTildeInPath
-	do {
-		try string.write(toFile: fullPath, atomically: false, encoding: .utf8)
-	} catch {
-		preconditionFailure("Failed to write to file \(fullPath)")
-	}
+fileprivate extension Geometry {
+    func save(to fileURL: URL, using environment: Environment) {
+        do {
+            let string = self.scadString(in: environment)
+            try string.write(to: fileURL, atomically: true, encoding: .utf8)
+        } catch {
+            preconditionFailure("Failed to write to file \(fileURL)")
+        }
+    }
+
+    func save(to path: String, using environment: Environment) {
+        let url = URL(fileURLWithPath: (path as NSString).expandingTildeInPath)
+        save(to: url, using: environment)
+    }
 }
 
 public extension Geometry3D {
-	@discardableResult func save(to path: String) -> Geometry3D {
-		let string = self
-            .usingDefaultFacets()
-            .scadString(in: Environment())
-		saveToFile(string: string, to: path)
-		return self
-	}
+    @discardableResult func save(to path: String) -> Geometry3D {
+        self.usingDefaultFacets().save(to: path, using: Environment())
+        return self
+    }
+
+    @discardableResult func save(to url: URL) -> Geometry3D {
+        self.usingDefaultFacets().save(to: url, using: Environment())
+        return self
+    }
 }
 
 public extension Geometry2D {
 	@discardableResult func save(to path: String) -> Geometry2D {
-		let string = self
-            .usingDefaultFacets()
-            .scadString(in: Environment())
-		saveToFile(string: string, to: path)
+        self.usingDefaultFacets().save(to: path, using: Environment())
 		return self
 	}
+
+    @discardableResult func save(to url: URL) -> Geometry2D {
+        self.usingDefaultFacets().save(to: url, using: Environment())
+        return self
+    }
 }
