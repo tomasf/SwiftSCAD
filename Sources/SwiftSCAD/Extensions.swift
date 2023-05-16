9773ffe1b619a94a4cda3e96a308e6b03465c1dd
Tomas Franzén <tomas@tomasf.se>
2023-05-16 19:12:35 +0200

    Simplify Array.paired()

--- Sources/SwiftSCAD/Extensions.swift
+++ Sources/SwiftSCAD/Extensions.swift
@@ -1,9 +1,8 @@
 import Foundation
 
-public extension Array {
+extension Array {
     func paired() -> [(Element, Element)] {
-        guard count >= 2 else { return [] }
-        return (0..<count-1).map { (self[$0], self[$0+1]) }
+        .init(zip(self, dropFirst()))
     }
 }
 
