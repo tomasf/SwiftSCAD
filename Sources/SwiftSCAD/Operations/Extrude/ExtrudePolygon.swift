a2557491632f0767e77375b2c2a081be21ec2123
Tomas Franzén <tomas@tomasf.se>
2024-02-24 18:05:09 +0100

    Add extrudedAlongHelix

--- Sources/SwiftSCAD/Operations/Extrude/ExtrudePolygon.swift
+++ Sources/SwiftSCAD/Operations/Extrude/ExtrudePolygon.swift
@@ -37,7 +37,7 @@ public extension Polygon {
     /// - Parameters:
     ///   - path: An array of affine transforms representing the path along which the polygon will be extruded.
     ///   - steps: The number of steps to divide the interpolation between each pair of transforms in the `path`. Defaults to 1.
-    ///   - convexity: The convexity of the resulting 3D geometry. Defaults to 2.
+    ///   - convexity: The maximum number of surfaces a straight line can intersect the result. This helps OpenSCAD preview the geometry correctly, but has no effect on final rendering.
     ///
     /// - Returns: A 3D geometry resulting from extruding the polygon along the specified path.
     ///
@@ -49,6 +49,26 @@ public extension Polygon {
             }
 
             Polyhedron(extruding: self, along: expandedPath, convexity: convexity, environment: environment)
+        }
+    }
+
+    /// Extrude the polygon along a circular helix around the Z axis
+    ///
+    /// - Parameters:
+    ///   - pitch: The Z distance between each turn of the helix
+    ///   - height: The total height of the helix
+    ///   - convexity: The maximum number of surfaces a straight line can intersect the result. This helps OpenSCAD preview the geometry correctly, but has no effect on final rendering.
+
+    func extrudedAlongHelix(pitch: Double, height: Double, convexity: Int = 2) -> any Geometry3D {
+        EnvironmentReader { environment in
+            let radius = boundingRect(in: environment).maximum.x
+            let stepsPerRev = Double(environment.facets.facetCount(circleRadius: radius))
+            let steps = Int(ceil(stepsPerRev * height / pitch))
+
+            self.extruded(along: (0..<steps).map { step in
+                    .rotation(x: 90°, z: Double(step) / stepsPerRev * 360°)
+                    .translated(z: Double(step) / stepsPerRev * pitch)
+            }, convexity: convexity)
         }
     }
 }
