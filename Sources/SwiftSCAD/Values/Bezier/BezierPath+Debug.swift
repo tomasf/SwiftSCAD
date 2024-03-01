274edaf3a521a5f6c50329d7a62a8af157ef07e6
Tomas Franzén <tomas@tomasf.se>
2024-03-01 20:00:02 +0100

    Add cross product and dot product operators

--- Sources/SwiftSCAD/Values/Bezier/BezierPath+Debug.swift
+++ Sources/SwiftSCAD/Values/Bezier/BezierPath+Debug.swift
@@ -68,15 +68,3 @@ extension BezierPath {
         }
     }
 }
-
-fileprivate extension Vector {
-    var vector3D: Vector3D {
-        if let v3d = self as? Vector3D {
-            return v3d
-        } else if let v2d = self as? Vector2D {
-            return .init(v2d, z: 0)
-        } else {
-            preconditionFailure()
-        }
-    }
-}
--- Sources/SwiftSCAD/Values/Vectors/Vector.swift
+++ Sources/SwiftSCAD/Values/Vectors/Vector.swift
@@ -24,3 +24,18 @@ public protocol Vector {
     func setting(axes: Axes, to: Double) -> Self
     subscript(_ axis: Axes.Axis) -> Double { get }
 }
+
+infix operator ×
+infix operator ⋅
+
+internal extension Vector {
+    var vector3D: Vector3D {
+        if let v3d = self as? Vector3D {
+            return v3d
+        } else if let v2d = self as? Vector2D {
+            return .init(v2d, z: 0)
+        } else {
+            preconditionFailure()
+        }
+    }
+}
