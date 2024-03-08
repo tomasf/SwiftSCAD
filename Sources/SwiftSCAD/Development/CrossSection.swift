35ae2a4168cd1e2f81d4d43ad4e8b47dc9f4c1bc
Tomas Franzén <tomas@tomasf.se>
2024-03-08 16:58:53 +0100

    Improve CrossSection
    Simplify the code, make it work with arbitrary axes and use intersection instead of difference to avoid clipping problems in preview mode

--- Sources/SwiftSCAD/Development/CrossSection.swift
+++ Sources/SwiftSCAD/Development/CrossSection.swift
@@ -1,26 +1,17 @@
 import Foundation
 
 struct CrossSection: Shape3D {
-    let axis: Axis3D
-    let positive: Bool
+    let axis: Vector3D
     let offset: Double
     let content: any Geometry3D
 
     var body: any Geometry3D {
         let universeLength = 1000.0
-        var y: Angle = 0°
-        var z: Angle = 0°
 
-        switch axis {
-        case .x: z = positive ? 0° : 180°
-        case .y: z = positive ? 90° : -90°
-        case .z: y = positive ? -90° : 90°
-        }
-
-        return content.subtracting {
-            Box([universeLength, universeLength, universeLength], center: [.y, .z])
-                .translated(x: positive ? offset : -offset)
-                .rotated(y: y, z: z)
+        return content.intersection {
+            Box([universeLength, universeLength, universeLength], center: .xy)
+                .translated(z: offset)
+                .transformed(.rotation(from: [0, 0, 1], to: axis))
                 .colored(.blue)
         }
     }
@@ -29,20 +20,39 @@ struct CrossSection: Shape3D {
 public extension Geometry3D {
     /// Create a cross-section of this geometry
     ///
-    /// A cross-section can be a useful way to debug and verify geometry. The cut is made so that the negative part of an axis is removed, leaving the positive direction visible, or the opposite if `invert` is true.
+    /// A cross-section can be a useful way to debug and verify geometry.
     ///
     /// - Parameters:
     ///   - axis: The axis along which to cut
     ///   - offset: The offset from zero at which to cut
-    ///   - invert: Normally, the cut is made so
-    ///   - rendered: Enable cross-section in render mode
+    ///   - cuttingAway: Which direction to cut. The default, .negative, cuts away towards the negative direction of the axis, leaving the positive part visible.
+    ///   - rendered: Enable cross-section in render mode. Defaults to false.
     /// - Returns: A cross-sectioned geometry
-    func crossSectioned(axis: Axis3D, offset: Double = 0, invert: Bool = false, rendered: Bool = false) -> any Geometry3D {
+    @UnionBuilder3D
+    func crossSectioned(axis: Axis3D, offset: Double = 0, cuttingAway axisDirection: AxisDirection = .negative, rendered: Bool = false) -> any Geometry3D {
+        let direction = axis.direction * (axisDirection == .positive ? -1 : 1)
+        crossSectioned(axis: direction, offset: offset, rendered: rendered)
+    }
+
+    /// Create a cross-section of this geometry
+    ///
+    /// A cross-section can be a useful way to debug and verify geometry.
+    ///
+    /// - Parameters:
+    ///   - axis: The axis along which to cut. The part facing this direction is visible, cutting away the rest.
+    ///   - offset: The offset from zero at which to cut
+    ///   - rendered: Enable cross-section in render mode. Defaults to false.
+    /// - Returns: A cross-sectioned geometry
+
+    @UnionBuilder3D
+    func crossSectioned(axis: Vector3D, offset: Double = 0, rendered: Bool = false) -> any Geometry3D {
+        let section = CrossSection(axis: axis, offset: offset, content: self)
+
         IfPreview {
-            CrossSection(axis: axis, positive: invert, offset: offset, content: self)
+            section
         } else: {
             if rendered {
-                CrossSection(axis: axis, positive: invert, offset: offset, content: self)
+                section
             } else {
                 self
             }
--- Sources/SwiftSCAD/Values/Axis/Axis.swift
+++ Sources/SwiftSCAD/Values/Axis/Axis.swift
@@ -11,4 +11,16 @@ public enum Axis3D: Int {
     case x
     case y
     case z
+
+    var direction: Vector3D {
+        Vector3D(axis: self, value: 1)
+    }
+}
+
+/// A direction along an axis
+public enum AxisDirection {
+    /// The positive direction along an axis
+    case positive
+    /// The negative direction along an axis
+    case negative
 }
