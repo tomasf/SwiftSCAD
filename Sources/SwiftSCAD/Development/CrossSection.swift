e237634ec02367d71ecaee02e7ede402c41a7c69
Tomas Franzén <tomas@tomasf.se>
2024-03-15 09:41:59 +0100

    Remove IfPreview

--- Sources/SwiftSCAD/Development/CrossSection.swift
+++ Sources/SwiftSCAD/Development/CrossSection.swift
@@ -26,12 +26,11 @@ public extension Geometry3D {
     ///   - axis: The axis along which to cut
     ///   - offset: The offset from zero at which to cut
     ///   - cuttingAway: Which direction to cut. The default, .negative, cuts away towards the negative direction of the axis, leaving the positive part visible.
-    ///   - rendered: Enable cross-section in render mode. Defaults to false.
     /// - Returns: A cross-sectioned geometry
     @UnionBuilder3D
-    func crossSectioned(axis: Axis3D, offset: Double = 0, cuttingAway axisDirection: AxisDirection = .negative, rendered: Bool = false) -> any Geometry3D {
+    func crossSectioned(axis: Axis3D, offset: Double = 0, cuttingAway axisDirection: AxisDirection = .negative) -> any Geometry3D {
         let direction = axis.direction * (axisDirection == .positive ? -1 : 1)
-        crossSectioned(axis: direction, offset: offset, rendered: rendered)
+        crossSectioned(axis: direction, offset: offset)
     }
 
     /// Create a cross-section of this geometry
@@ -41,22 +40,10 @@ public extension Geometry3D {
     /// - Parameters:
     ///   - axis: The axis along which to cut. The part facing this direction is visible, cutting away the rest.
     ///   - offset: The offset from zero at which to cut
-    ///   - rendered: Enable cross-section in render mode. Defaults to false.
     /// - Returns: A cross-sectioned geometry
 
     @UnionBuilder3D
-    func crossSectioned(axis: Vector3D, offset: Double = 0, rendered: Bool = false) -> any Geometry3D {
-        let section = CrossSection(axis: axis, offset: offset, content: self)
-
-        #warning("fix")
-        //IfPreview {
-            section
-       /* } else: {
-            if rendered {
-                section
-            } else {
-                self
-            }
-        }*/
+    func crossSectioned(axis: Vector3D, offset: Double = 0) -> any Geometry3D {
+        CrossSection(axis: axis, offset: offset, content: self)
     }
 }
