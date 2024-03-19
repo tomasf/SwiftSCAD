8d09a205a1f9562c180ea8f9a49463f44d5043ad
Tomas Franzén <tomas@tomasf.se>
2024-03-19 17:09:02 +0100

    Add docs for forceRendered

--- Sources/SwiftSCAD/Development/ForceRender.swift
+++ Sources/SwiftSCAD/Development/ForceRender.swift
@@ -19,12 +19,24 @@ struct ForceRender3D: WrappedGeometry3D {
 }
 
 public extension Geometry2D {
+    /// Force rendering
+    ///
+    /// In preview mode, this operation forces the generation of a mesh for this geometry. This approach is useful for ensuring accurate previews of geometries, especially when boolean operations are complex and slow to compute. It also helps in avoiding or working around preview artifacts that might arise.
+    ///
+    /// - Parameters:
+    ///   - convexity: The maximum number of surfaces a straight line can intersect the result. This helps OpenSCAD preview the geometry correctly, but has no effect on final rendering.
     func forceRendered(convexity: Int = 2) -> any Geometry2D {
         ForceRender2D(body: self, convexity: convexity)
     }
 }
 
 public extension Geometry3D {
+    /// Force rendering
+    ///
+    /// In preview mode, this operation forces the generation of a mesh for this geometry. This approach is useful for ensuring accurate previews of geometries, especially when boolean operations are complex and slow to compute. It also helps in avoiding or working around preview artifacts that might arise.
+    ///
+    /// - Parameters:
+    ///   - convexity: The maximum number of surfaces a straight line can intersect the result. This helps OpenSCAD preview the geometry correctly, but has no effect on final rendering.
     func forceRendered(convexity: Int = 2) -> any Geometry3D {
         ForceRender3D(body: self, convexity: convexity)
     }
