9ad1dc0546c4f5a1d5b412829924e784ce58ab46
Tomas Franzén <tomas@tomasf.se>
2024-03-19 16:59:36 +0100

    Add documentation for Minkowski sum

--- Sources/SwiftSCAD/Operations/Offsetting/MinkowskiSum.swift
+++ Sources/SwiftSCAD/Operations/Offsetting/MinkowskiSum.swift
@@ -26,14 +26,26 @@ struct Minkowski3D: Geometry3D {
     }
 }
 
-public extension Geometry3D {
-    func minkowskiSum(@SequenceBuilder3D adding other: () -> [any Geometry3D]) -> any Geometry3D {
-        Minkowski3D(children: [self] + other())
+public extension Geometry2D {
+    /// Combines the current geometry with one or more other geometries using the Minkowski sum operation.
+    ///
+    /// The Minkowski sum operation creates a new geometry that represents the combination of this geometry with each of the geometries provided by the `adding` closure. This operation is akin to blending the spatial features of the geometries, where the resulting shape embodies the outline that would be traced by moving one shape around the perimeter of the other.
+    ///
+    /// - Parameter adding: The 3D geometries to combine with this geometry.
+    /// - Returns: A geometry representing the Minkowski sum
+    func minkowskiSum(@SequenceBuilder2D adding other: () -> [any Geometry2D]) -> any Geometry2D {
+        Minkowski2D(children: [self] + other())
     }
 }
 
-public extension Geometry2D {
-    func minkowskiSum(@SequenceBuilder2D adding other: () -> [any Geometry2D]) -> any Geometry2D {
-        Minkowski2D(children: [self] + other())
+public extension Geometry3D {
+    /// Combines the current geometry with one or more other geometries using the Minkowski sum operation.
+    ///
+    /// The Minkowski sum operation creates a new geometry that represents the combination of this geometry with each of the geometries provided by the `adding` closure. This operation is akin to blending the spatial features of the geometries, where the resulting shape embodies the outline that would be traced by moving one shape around the perimeter of the other.
+    ///
+    /// - Parameter adding: The 3D geometries to combine with this geometry.
+    /// - Returns: A geometry representing the Minkowski sum
+    func minkowskiSum(@SequenceBuilder3D adding other: () -> [any Geometry3D]) -> any Geometry3D {
+        Minkowski3D(children: [self] + other())
     }
 }
