24597cfa3856d5b1016bdcd21b715c29922e6f7e
Tomas Franzén <tomas@tomasf.se>
2024-05-18 14:34:17 +0200

    Document RectangleCorners and add a ~ prefix operator

--- Sources/SwiftSCAD/Values/Corners/RectangleCorners.swift
+++ Sources/SwiftSCAD/Values/Corners/RectangleCorners.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+/// A structure that defines the corners of a rectangle using an option set.
+///
+/// `RectangleCorners` allows for specifying and manipulating individual corners or groups of corners of a rectangle.
+/// It is used for operations that involve corner-specific modifications, such as rounding corners.
+
 public struct RectangleCorners: OptionSet, Hashable {
     public let rawValue: Int
 
@@ -12,11 +17,32 @@ public struct RectangleCorners: OptionSet, Hashable {
     public static let topRight = Self(rawValue: 1 << 2)
     public static let bottomRight = Self(rawValue: 1 << 3)
 
+    /// A set containing all corners of the rectangle.
     public static let all: RectangleCorners = [.bottomLeft, .bottomRight, .topRight, .topLeft]
+
+    /// A set containing none of the corners.
     public static let none: RectangleCorners = []
 
     public static let top: RectangleCorners = [.topRight, .topLeft]
     public static let bottom: RectangleCorners = [.bottomLeft, .bottomRight]
     public static let left: RectangleCorners = [.bottomLeft, .topLeft]
     public static let right: RectangleCorners = [.bottomRight, .topRight]
+
+    /// Returns a new set of rectangle corners with the specified corners inverted.
+    ///
+    /// This operator returns a set containing all corners that are not in the original set.
+    ///
+    /// - Parameter corners: The set of corners to invert.
+    /// - Returns: A new set of rectangle corners with the specified corners inverted.
+    ///
+    /// Example usage:
+    /// ```swift
+    /// let corners: RectangleCorners = [.topLeft, .bottomRight]
+    /// let notTopLeftOrBottomRight = ~corners
+    /// // notTopLeftOrBottomRight contains [.bottomLeft, .topRight]
+    /// ```
+
+    public static prefix func ~(_ corners: RectangleCorners) -> RectangleCorners {
+        .all.subtracting(corners)
+    }
 }
