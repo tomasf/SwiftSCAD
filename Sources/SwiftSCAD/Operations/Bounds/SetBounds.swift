faa30e4dac93f36fc25b8e4e085af144b5f18496
Tomas Franzén <tomas@tomasf.se>
2024-06-25 21:45:46 +0200

    Add settingBounds methods with a non-closure shape parameters

--- Sources/SwiftSCAD/Operations/Bounds/SetBounds.swift
+++ Sources/SwiftSCAD/Operations/Bounds/SetBounds.swift
@@ -23,18 +23,27 @@ struct SetBounds3D: WrappedGeometry3D {
 }
 
 public extension Geometry2D {
-    /// Sets the bounds of the geometry to the specified 2D bounding box.
+    /// Defines the bounds of the geometry as the specified 2D bounding box.
     /// - Parameter box: The bounding box to set for the geometry.
     /// - Returns: A  geometry with the specified bounds.
     func settingBounds(_ box: BoundingBox2D) -> any Geometry2D {
         SetBounds2D(body: self, boundary: .init(boundingBox: box))
     }
 
-    /// Sets the bounds of the geometry based on a custom 2D shape defined using a closure.
-    /// - Parameter shape: A closure that returns the custom 2D geometry shape.
+    /// Defines the bounds of the geometry based on a custom 2D shape.
+    /// - Parameter shape: The shape whose bounds you want to use.
     /// - Returns: A geometry with the bounds obtained from the custom shape.
     func settingBounds(@UnionBuilder2D _ shape: () -> any Geometry2D) -> any Geometry2D {
         ReadBoundary2D(body: shape()) { _, boundary in
+            SetBounds2D(body: self, boundary: boundary)
+        }
+    }
+
+    /// Defines the bounds of the geometry based on a custom 2D shape.
+    /// - Parameter shape: The shape whose bounds you want to use.
+    /// - Returns: A geometry with the bounds obtained from the custom shape.
+    func settingBounds(_ shape: any Geometry2D) -> any Geometry2D {
+        ReadBoundary2D(body: shape) { _, boundary in
             SetBounds2D(body: self, boundary: boundary)
         }
     }
@@ -53,18 +62,27 @@ public extension Geometry2D {
 }
 
 public extension Geometry3D {
-    /// Sets the bounds of the geometry to the specified 3D bounding box.
+    /// Defines the bounds of the geometry as the specified 3D bounding box.
     /// - Parameter box: The bounding box to set for the geometry.
     /// - Returns: A  geometry with the specified bounds.
     func settingBounds(_ box: BoundingBox3D) -> any Geometry3D {
         SetBounds3D(body: self, boundary: .init(boundingBox: box))
     }
 
-    /// Sets the bounds of the geometry based on a custom 3D shape defined using a closure.
-    /// - Parameter shape: A closure that returns the custom 3D geometry shape.
+    /// Defines the bounds of the geometry based on a custom 3D shape.
+    /// - Parameter shape: The shape whose bounds you want to use.
     /// - Returns: A geometry with the bounds obtained from the custom shape.
     func settingBounds(@UnionBuilder3D _ shape: () -> any Geometry3D) -> any Geometry3D {
         ReadBoundary3D(body: shape()) { _, boundary in
+            SetBounds3D(body: self, boundary: boundary)
+        }
+    }
+
+    /// Defines the bounds of the geometry based on a custom 3D shape.
+    /// - Parameter shape: The shape whose bounds you want to use.
+    /// - Returns: A geometry with the bounds obtained from the custom shape.
+    func settingBounds(_ shape: any Geometry3D) -> any Geometry3D {
+        ReadBoundary3D(body: shape) { _, boundary in
             SetBounds3D(body: self, boundary: boundary)
         }
     }
