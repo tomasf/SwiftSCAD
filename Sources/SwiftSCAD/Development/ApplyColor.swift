9dba7836e1d4ed80a23af8a9baeafe3b624d8bd0
Tomas Franzén <tomas@tomasf.se>
2024-07-28 18:32:56 +0200

    Improve Color

--- Sources/SwiftSCAD/Development/ApplyColor.swift
+++ Sources/SwiftSCAD/Development/ApplyColor.swift
@@ -1,21 +1,5 @@
 import Foundation
 
-internal extension Color {
-    var invocation: Invocation {
-        let params: [String: any SCADValue]
-
-        switch self {
-        case .components (let red, let green, let blue, let alpha):
-            params = ["c": [red, green, blue, alpha]]
-
-        case .named (let name, let alpha):
-            params = ["c": name.rawValue, "alpha": alpha]
-        }
-
-        return Invocation(name: "color", parameters: params)
-    }
-}
-
 struct ApplyColor2D: WrappedGeometry2D {
     let color: Color
     let body: any Geometry2D
@@ -33,13 +17,8 @@ extension Geometry2D {
         ApplyColor2D(color: color, body: self)
     }
 
-    /// Apply a color to the geometry
-    /// - Parameters:
-    ///   - name: One of the standard colors. See [the OpenSCAD docs](https://en.wikibooks.org/wiki/OpenSCAD_User_Manual/Transformations#color) for more info.
-    ///   - alpha: The alpha value of the color, in the range 0.0 to 1.0.
-    /// - Returns: A colored geometry
-    public func colored(_ name: Color.Name, alpha: Double = 1) -> any Geometry2D {
-        ApplyColor2D(color: .named(name, alpha: alpha), body: self)
+    public func colored(_ color: Color, alpha: Double) -> any Geometry2D {
+        ApplyColor2D(color: color.withAlphaComponent(alpha), body: self)
     }
 
     /// Apply a color to the geometry
@@ -50,7 +29,7 @@ extension Geometry2D {
     ///   - alpha: The alpha component, in the range 0.0 to 1.0.
     /// - Returns: A colored geometry
     public func colored(red: Double, green: Double, blue: Double, alpha: Double = 1) -> any Geometry2D {
-        ApplyColor2D(color: .components(red: red, green: green, blue: blue, alpha: alpha), body: self)
+        ApplyColor2D(color: .init(red: red, green: green, blue: blue, alpha: alpha), body: self)
     }
 }
 
@@ -59,13 +38,8 @@ extension Geometry3D {
         ApplyColor3D(color: color, body: self)
     }
 
-    /// Apply a color to the geometry
-    /// - Parameters:
-    ///   - name: One of the standard colors. See [the OpenSCAD docs](https://en.wikibooks.org/wiki/OpenSCAD_User_Manual/Transformations#color) for more info.
-    ///   - alpha: The alpha value of the color, in the range 0.0 to 1.0.
-    /// - Returns: A colored geometry
-    public func colored(_ name: Color.Name, alpha: Double = 1) -> any Geometry3D {
-        ApplyColor3D(color: .named(name, alpha: alpha), body: self)
+    public func colored(_ color: Color, alpha: Double) -> any Geometry3D {
+        ApplyColor3D(color: color.withAlphaComponent(alpha), body: self)
     }
 
     /// Apply a color to the geometry
@@ -76,6 +50,6 @@ extension Geometry3D {
     ///   - alpha: The alpha component, in the range 0.0 to 1.0.
     /// - Returns: A colored geometry
     public func colored(red: Double, green: Double, blue: Double, alpha: Double = 1) -> any Geometry3D {
-        ApplyColor3D(color: .components(red: red, green: green, blue: blue, alpha: alpha), body: self)
+        ApplyColor3D(color: .init(red: red, green: green, blue: blue, alpha: alpha), body: self)
     }
 }
