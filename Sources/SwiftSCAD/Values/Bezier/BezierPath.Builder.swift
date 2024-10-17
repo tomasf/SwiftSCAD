755e4b34cf6c1e71c59ae2c5ca910f9590c91c41
Tomas Franzén <tomas@tomasf.se>
2024-10-17 21:35:54 +0200

    Add BezierPath.Builder

---
+++ Sources/SwiftSCAD/Values/Bezier/BezierPath.Builder.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+public extension BezierPath {
+    @resultBuilder struct Builder {
+        public typealias Component = BezierPath<V>.Component
+
+        public static func buildExpression(_ expression: Component) -> [Component] {
+            [expression]
+        }
+
+        public static func buildBlock(_ children: [Component]...) -> [Component] {
+            children.flatMap { $0 }
+        }
+
+        // If without else
+        public static func buildOptional(_ children: [Component]?) -> [Component] {
+            if let children {
+                [.init(group: children)]
+            } else {
+                []
+            }
+        }
+
+        // If
+        public static func buildEither(first child: [Component]) -> [Component] {
+            [.init(group: child)]
+        }
+
+        // Else
+        public static func buildEither(second child: [Component]) -> [Component] {
+            [.init(group: child)]
+        }
+
+        // Loops
+        public static func buildArray(_ children: [[Component]]) -> [Component] {
+            children.flatMap { $0 }
+        }
+
+        public static func buildExpression(_ void: Void) -> [Component] { [] }
+        public static func buildExpression(_ never: Never) -> [Component] {}
+    }
+}
+
+public extension BezierPath {
+    enum Positioning {
+        case relative
+        case absolute
+    }
+
+    init(from: V = .zero, _ positioning: Positioning, @Builder builder: () -> [Component]) {
+        var start = from
+        self.init(startPoint: from, curves: builder().flatMap {
+            $0.bezierCurves(start: &start, positioning: positioning)
+        })
+    }
+}
