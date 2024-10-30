36c8cb8466071f06b5afef8952ce307fb5b463d0
Tomas Franzén <tomas@tomasf.se>
2024-10-30 13:35:21 +0100

    Add improved replaced() methods

--- Sources/SwiftSCAD/Operations/Replace.swift
+++ Sources/SwiftSCAD/Operations/Replace.swift
@@ -1,13 +1,37 @@
 import Foundation
 
 public extension Geometry2D {
-    func replaced(@GeometryBuilder2D with replacement: (_ input: any Geometry2D) -> any Geometry2D) -> any Geometry2D {
-        replacement(self)
+    func replaced(if condition: Bool = true, @GeometryBuilder2D with replacement: (_ input: any Geometry2D) -> any Geometry2D) -> any Geometry2D {
+        if condition {
+            replacement(self)
+        } else {
+            self
+        }
+    }
+
+    func replaced<T>(if optional: T?, @GeometryBuilder2D with replacement: (_ input: any Geometry2D, _ value: T) -> any Geometry2D) -> any Geometry2D {
+        if let optional {
+            replacement(self, optional)
+        } else {
+            self
+        }
     }
 }
 
 public extension Geometry3D {
-    func replaced(@GeometryBuilder3D with replacement: (_ input: any Geometry3D) -> any Geometry3D) -> any Geometry3D {
-        replacement(self)
+    func replaced(if condition: Bool = true, @GeometryBuilder3D with replacement: (_ input: any Geometry3D) -> any Geometry3D) -> any Geometry3D {
+        if condition {
+            replacement(self)
+        } else {
+            self
+        }
+    }
+
+    func replaced<T>(if optional: T?, @GeometryBuilder3D with replacement: (_ input: any Geometry3D, _ value: T) -> any Geometry3D) -> any Geometry3D {
+        if let optional {
+            replacement(self, optional)
+        } else {
+            self
+        }
     }
 }
