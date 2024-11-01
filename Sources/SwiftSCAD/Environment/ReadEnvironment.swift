de04e409da4bb8a03b24efc6f5b948ffda87bd67
Tomas Franzén <tomas@tomasf.se>
2024-11-01 12:12:54 +0100

    Add readEnvironment with key paths

--- Sources/SwiftSCAD/Environment/ReadEnvironment.swift
+++ Sources/SwiftSCAD/Environment/ReadEnvironment.swift
@@ -43,6 +43,36 @@ public func readEnvironment(@GeometryBuilder2D _ body: @escaping (Environment) -
     _EnvironmentReader(body: body)
 }
 
+public func readEnvironment<T>(
+    _ keyPath1: KeyPath<Environment, T>,
+    @GeometryBuilder2D _ body: @escaping (T) -> any Geometry2D
+) -> any Geometry2D {
+    readEnvironment {
+        body($0[keyPath: keyPath1])
+    }
+}
+
+public func readEnvironment<T, U>(
+    _ keyPath1: KeyPath<Environment, T>,
+    _ keyPath2: KeyPath<Environment, U>,
+    @GeometryBuilder2D _ body: @escaping (T, U) -> any Geometry2D
+) -> any Geometry2D {
+    readEnvironment {
+        body($0[keyPath: keyPath1], $0[keyPath: keyPath2])
+    }
+}
+
+public func readEnvironment<T, U, V>(
+    _ keyPath1: KeyPath<Environment, T>,
+    _ keyPath2: KeyPath<Environment, U>,
+    _ keyPath3: KeyPath<Environment, V>,
+    @GeometryBuilder2D _ body: @escaping (T, U, V) -> any Geometry2D
+) -> any Geometry2D {
+    readEnvironment {
+        body($0[keyPath: keyPath1], $0[keyPath: keyPath2], $0[keyPath: keyPath3])
+    }
+}
+
 /// Creates a geometry that can read and respond to the current environment settings.
 ///
 /// Use this function to create a geometry that has access to environmental information. This allows for dynamic and conditional geometry creation based on the current environment settings such as facets, text settings, or custom values you've defined.
@@ -51,4 +81,34 @@ public func readEnvironment(@GeometryBuilder2D _ body: @escaping (Environment) -
 /// - Returns: A geometry instance that can be dynamically created based on the current environment.
 public func readEnvironment(@GeometryBuilder3D _ body: @escaping (Environment) -> any Geometry3D) -> any Geometry3D {
     _EnvironmentReader(body: body)
+}
+
+public func readEnvironment<T>(
+    _ keyPath1: KeyPath<Environment, T>,
+    @GeometryBuilder3D _ body: @escaping (T) -> any Geometry3D
+) -> any Geometry3D {
+    readEnvironment {
+        body($0[keyPath: keyPath1])
+    }
+}
+
+public func readEnvironment<T, U>(
+    _ keyPath1: KeyPath<Environment, T>,
+    _ keyPath2: KeyPath<Environment, U>,
+    @GeometryBuilder3D _ body: @escaping (T, U) -> any Geometry3D
+) -> any Geometry3D {
+    readEnvironment {
+        body($0[keyPath: keyPath1], $0[keyPath: keyPath2])
+    }
+}
+
+public func readEnvironment<T, U, V>(
+    _ keyPath1: KeyPath<Environment, T>,
+    _ keyPath2: KeyPath<Environment, U>,
+    _ keyPath3: KeyPath<Environment, V>,
+    @GeometryBuilder3D _ body: @escaping (T, U, V) -> any Geometry3D
+) -> any Geometry3D {
+    readEnvironment {
+        body($0[keyPath: keyPath1], $0[keyPath: keyPath2], $0[keyPath: keyPath3])
+    }
 }
