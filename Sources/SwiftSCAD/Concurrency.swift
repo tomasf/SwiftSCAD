42503ce585e57ca8f89a0693bb2586102d47ac14
Tomas Franzén <tomas@tomasf.se>
2024-11-05 15:13:36 +0100

    Try using GCD for concurrentForEach

--- Sources/SwiftSCAD/Concurrency.swift
+++ Sources/SwiftSCAD/Concurrency.swift
@@ -2,22 +2,9 @@ import Foundation
 
 internal extension Collection where Self: Sendable, Element: Sendable {
     func concurrentForEach(_ body: @Sendable @escaping (Element) -> Void) {
-        if #available(macOS 10.15, *) {
-            let semaphore = DispatchSemaphore(value: 0)
-
-            Task {
-                await withTaskGroup(of: Void.self) { taskGroup in
-                    for element in self {
-                        taskGroup.addTask {
-                            body(element)
-                        }
-                    }
-                }
-                semaphore.signal()
-            }
-            semaphore.wait()
-        } else {
-            forEach(body)
+        DispatchQueue.concurrentPerform(iterations: count) { i in
+            let element = self[index(startIndex, offsetBy: i)]
+            body(element)
         }
     }
 }
