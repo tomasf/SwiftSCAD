a7d2cce1d7e70fd7fe3696e1b85389fd6a76f0dd
Tomas Franzén <tomas@tomasf.se>
2024-11-14 21:50:40 +0100

    Add documentation for Environment

--- Sources/SwiftSCAD/Environment/Environment.swift
+++ Sources/SwiftSCAD/Environment/Environment.swift
@@ -1,5 +1,21 @@
 import Foundation
 
+/// A property wrapper for reading values from `EnvironmentValues` within the context of a `Shape2D` or `Shape3D`.
+///
+/// Use `@Environment` to access values that are stored in the current environment, such as configuration or styling options.
+/// It reads the specified key path from `EnvironmentValues`, or if none is specified, retrieves the entire `EnvironmentValues`.
+///
+/// - Important: Accessing an `@Environment` property outside the `body` property of a Shape is unsupported and returns a default value.
+///
+/// ## Usage
+/// ```swift
+/// @Environment(\.tolerance) var tolerance  // Reads a specific value from the environment
+/// @Environment var environment             // Reads the entire environment
+/// ```
+///
+/// - Parameters:
+///   - keyPath: A key path to the value in `EnvironmentValues`, which determines the specific value to read.
+///
 @propertyWrapper public struct Environment<T> {
     private let keyPath: KeyPath<EnvironmentValues, T>
 
