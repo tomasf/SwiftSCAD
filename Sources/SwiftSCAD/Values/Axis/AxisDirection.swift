c66af7ef09af5894337890a7d2ed48c85742abb2
Tomas Franzén <tomas@tomasf.se>
2024-11-23 13:28:12 +0100

    Clarify .min and .max

--- Sources/SwiftSCAD/Values/Axis/AxisDirection.swift
+++ Sources/SwiftSCAD/Values/Axis/AxisDirection.swift
@@ -7,7 +7,14 @@ public enum AxisDirection: Sendable, Hashable, CaseIterable {
     /// The negative direction along an axis (pointing towards negative infinity)
     case negative
 
+    /// Represents the minimum direction along an axis, which corresponds to the `.negative` direction.
+    /// This alias can be used to improve readability in contexts where "minimum" or "low" values
+    /// are conceptually aligned with the `.negative` direction.
     public static let min = negative
+
+    /// Represents the maximum direction along an axis, which corresponds to the `.positive` direction.
+    /// This alias can be used to improve readability in contexts where "maximum" or "high" values
+    /// are conceptually aligned with the `.positive` direction.
     public static let max = positive
 
     internal var factor: Double {
